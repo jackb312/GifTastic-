dc1d01e44c83ff8dc10e66df50ff0a569e88320a
Jack Brettell <jack.brettell@gmail.com>
2019-09-02 22:13:12 -0700

    working js, still need styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,86 @@
-var topics = ["cats", "dogs", "elephants", "lions", "bears", "cheetahs", "tigers", 
-"monkeys", "sharks", "orcas", "seals", "salmon", "penguins"];
-renderButtons();
-function renderButtons() {
-    $("#buttons").empty();
-    for (var i = 0; i < topics.length; i++) {
-        var a = $("<button class='btn-primary'>");
-        a.addClass("topicsChar");
-        a.attr("data-animals", topics[i]);
-        a.text(topics[i]);
-        $("#buttons").append(a);
-    }
-};
 $(document).ready(function() {
-    $("#buttons").on("click", "animalsChar", function(){
-        console.log("button has clicked");
-        var topicsChar = $(this).attr("data-animals");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topicsChar +
-        "&api_key=Rq2jacKO33rCa1wdRmGCC3qQJAWyyrDG&limit=10";
+    var topics = ["lion", "tiger", "bear", "falcon", "seahawk", "salmon", "orca", "dolphin", 
+    "elephant", "monkey", "snake", "shark"];
+    function displayImg(){
+
+        $("#animal-images").empty();
+        var input = $(this).attr("data-name");
+        var limit = 10;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&limit=" + limit + "&api_key=Rq2jacKO33rCa1wdRmGCC3qQJAWyyrDG";   
+
         $.ajax({
-            url: queryURL,
+            url: queryURL, 
             method: "GET"
-        })
-    .done(function(response){
-        var results = response.data;
-        for (var i = 0; i < results.length; i++) {
-            if (topics[i].rating !== "r" && topics[i].rating !== "pg-13") {
-                var gifDiv = $("<div class = 'gifBucket'>");
-                var rating = results[i].rating;
-                var p = $("<p>").text("Rating: " + rating);
-                var topicsImage = $("<img>");
-                topicsImage.attr("class", "gif");
-                topicsImage.attr("src", results[i].images.fixed_height_still.url);
-                topicsImage.attr("data-still", results[i].images.fixed_height_still.url);
-                topicsImage.attr("data-animate", results[i].images.fixed_height.url);
-                topicsImage.attr("data-state", "still")
-                gifDiv.append(topicsImage);
-                gifDiv.append(p);
-                $("#gifs-area").prepend(gifDiv);
+        }).done(function(response) {
+
+            for(var j = 0; j < limit; j++) {    
+
+                var displayDiv = $("<div>");
+                displayDiv.addClass("holder");
+            
+                var image = $("<img>");
+                image.attr("src", response.data[j].images.original_still.url);
+                image.attr("data-still", response.data[j].images.original_still.url);
+                image.attr("data-animate", response.data[j].images.original.url);
+                image.attr("data-state", "still");
+                image.attr("class", "gif");
+                displayDiv.append(image);
+
+                var rating = response.data[j].rating;
+                console.log(response);
+                var pRating = $("<p>").text("Rating: " + rating);
+                displayDiv.append(pRating)
+
+                $("#animal-images").append(displayDiv);
             }
+        });
+    }
+
+    function renderButtons(){ 
+
+        $("#animal-buttons").empty();
+
+        for (var i = 0; i < topics.length; i++){
+
+            var newButton = $("<button>") 
+            newButton.attr("class", "btn btn-default");
+            newButton.attr("id", "input")  
+            newButton.attr("data-name", topics[i]); 
+            newButton.text(topics[i]); 
+            $("#animal-buttons").append(newButton); 
         }
-    });
-    });
-    $(document).on("click", ".gif", function(){
+    }
+
+    function imageChangeState() {          
+
         var state = $(this).attr("data-state");
-        console.log("gif clicked state= " + state);
-        if (state === "still") {
-            $(this).attr("src", $(this).attr("data-animate"));
-            $(this).attr("data-state", "animate"); 
-            } else {
-            $(this).attr("src", $(this).attr("data-still"));
+        var animateImage = $(this).attr("data-animate");
+        var stillImage = $(this).attr("data-still");
+
+        if(state == "still") {
+            $(this).attr("src", animateImage);
+            $(this).attr("data-state", "animate");
+        }
+
+        else if(state == "animate") {
+            $(this).attr("src", stillImage);
             $(this).attr("data-state", "still");
-            };
-    });
-    $("gifAdder").on("click", function(){
-        console.log("Submit Pressed");
-        var newTopics = $("#addGifs").val();
-        if (newTopics !== "") {
-            topics.push(newTopics);
-            renderButtons();
-        };
-    });
+        }   
+    }
+
+    $("#add-animal").on("click", function(){
+
+        var newTopic = $("#animal-input").val().trim();
+        form.reset();
+        topics.push(newTopic);
+                
+        renderButtons();
+
+        return false;
+    })
+
+    renderButtons();
+
+    $(document).on("click", "#input", displayImg);
+    $(document).on("click", ".gif", imageChangeState);
 });
